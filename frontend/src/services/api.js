39d318074a3a0b254296f679bf9ff5f4c14d21da
Udit <145645636+uditttttt@users.noreply.github.com>
2025-08-18 20:49:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The base URL of our backend API
-const API_URL = 'http://localhost:5000/api/';
+const API_URL = 'https://expense-tracker-app-backend-zd41.onrender.com/api/';
 
 // Create a pre-configured instance of axios
 const api = axios.create({
